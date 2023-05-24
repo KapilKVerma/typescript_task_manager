cb47b807c903c4f95a3a9761c933b1adf6e136fc
Kapil Verma <kapilverma@Kapils-MacBook-Pro.local>
2023-05-24 21:43:48 +1000

    make connection to mongodb

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello")
+require("dotenv").config()
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+const PORT = config.get('port')
+
+app.listen(PORT,()=>{console.log(`App started at http://localhost:${PORT}`)})
